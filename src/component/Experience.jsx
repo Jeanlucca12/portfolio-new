5de9df4c15985f81310be879630cf811c9090a60
JeanLucca12 <jeanluccabueno@gmail.com>
2024-08-18 20:34:04 -0300

    edit services section

--- src/component/Experience.jsx
+++ src/component/Experience.jsx
@@ -1,56 +1,79 @@
-import React from 'react';
-import {EXPERIENCES} from '../constants/index'  
-import { animate, motion } from "framer-motion";
+import { FaCode } from "react-icons/fa";
+import { FaPencilRuler } from "react-icons/fa";
+import { FaMobile } from "react-icons/fa6";
+import { FaLightbulb } from "react-icons/fa";
 
-const Experience = () => {
+export default function Experience() {
   return (
-      <div className="border-b border-neutral-800 pb-16">
-        <motion.h2 
-        whileInView={{opacity:1,y:0}}
-        initial={{opacity:0, y:100}}
-        transition={{duration:0.5, delay:0.5}}
-        className='my-20 text-center text-violet-400 text-3xl font-light capitalize'>
-        experience
-        </motion.h2 >
-        {EXPERIENCES.map((ele,i)=>{
-        return<> 
-        <div key={i} className='mb-10 flex flex-wrap lg:justify-center'>
-          <motion.div
-           whileInView={{opacity:1,y:0}}
-           initial={{opacity:0, y:100}}
-           transition={{duration:0.5, delay:0.5}}
-          className='w-full lg:w-1/4 mb-2 '>
-            <p className='font-semibold text-sm opacity-60 text-purple-500'>{ele.year}</p>
-          </motion.div>
-          <div className='w-full max-w-xl text-lg lg:w-3/4'>
-            <motion.h6 
-             whileInView={{opacity:1,y:0}}
-             initial={{opacity:0, y:100}}
-             transition={{duration:0.5, delay:0.5}}
-            className='mb-2 text-md font-thin lg:text-lg'>{ele.role}   - 
-            <span className='text-sm opacity-70 text-purple-200'> {ele.company}</span>
-            </motion.h6>
-            <motion.p 
-             whileInView={{opacity:1,y:0}}
-             initial={{opacity:0, y:100}}
-             transition={{duration:0.5, delay:0.5}}
-            className='mb-2 opacity-70 text-sm tracking-tight font-light lg:text-xl'>
-              {ele.description}
-            </motion.p>
-            {ele.technologies.map((tech,index)=>
-              <motion.span 
-              whileInView={{opacity:1,y:0}}
-              initial={{opacity:0, y:100}}
-              transition={{duration:0.5, delay:0.5}}
-              key={index} className="ml-2 px-2 py-1 mt-2 text-sm bg-neutral-800 text-purple-400 opacity-55 rounded-xl">
-                {tech}
-              </motion.span>
-            )}
+    <section className="border-b border-neutral-800 pb-20">
+      <div className="container px-6 py-10 mx-auto">
+      <h1 className=" text-white text-4xl text-center my-8">
+        See
+        <span className="text-neutral-400"> my services</span>
+      </h1>
+
+        <div className="mt-2 flex justify-center">
+          <span className="inline-block w-40 h-1 bg-blue-500 rounded-full"></span>
+          <span className="inline-block w-3 h-1 ml-1 bg-blue-500 rounded-full"></span>
+          <span className="inline-block w-1 h-1 ml-1 bg-blue-500 rounded-full"></span>
+        </div>
+
+        <div className="mt-8 xl:mt-12 lg:flex lg:items-center">
+          <div className="mt-6 grid w-full grid-cols-1 gap-8 lg:w-1/2 xl:gap-16 md:grid-cols-2">
+            <div className="space-y-3">
+              <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-xl dark:text-white dark:bg-blue-500">
+                <FaCode className="w-6 h-6"/>
+              </span>
+
+              <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">Web Development</h1>
+
+              <p className="text-gray-500 dark:text-gray-300">
+              Creation of modern and responsive websites, using the latest web technologies to guarantee performance, security and an exceptional user experience.
+              </p>
+            </div>
+
+            <div className="space-y-3">
+              <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-xl dark:text-white dark:bg-blue-500">
+                <FaPencilRuler className="w-6 h-6"/>
+              </span>
+
+              <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">UX/UI Design</h1>
+
+              <p className="text-gray-500 dark:text-gray-300">
+              Interface designs that combine beauty and functionality, focused on the user experience to create digital products that are intuitive and pleasant to use.
+              </p>
+            </div>
+
+            <div className="space-y-3">
+              <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-xl dark:text-white dark:bg-blue-500">
+                <FaMobile className="h-6 w-6"/>
+              </span>
+
+              <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">Mobile Design</h1>
+
+              <p className="text-gray-500 dark:text-gray-300">
+              Design of mobile interfaces optimized for different devices, ensuring a fluid and pleasant experience on any screen.
+              </p>
+            </div>
+
+            <div className="space-y-3">
+              <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-xl dark:text-white dark:bg-blue-500">
+                <FaLightbulb className="w-6 h-6"/>
+              </span>
+
+              <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">Ideas and adjustments</h1>
+
+              <p className="text-gray-500 dark:text-gray-300">
+              Turn your ideas into reality! I offer support for adjustments, improvements and implementation of new features in your digital project.
+              </p>
+            </div>
+          </div>
+
+          <div className="mt-6 hidden lg:flex lg:w-1/2 lg:justify-center">
+            <img className="w-[28rem] h-[28rem] flex-shrink-0 object-cover xl:w-[28rem] xl:h-[28rem] rounded-full" src="/public/service.jpg" alt=""/>
           </div>
         </div>
-        </>
-        })}
       </div>
-  )
+    </section>
+  );
 }
-export default Experience
